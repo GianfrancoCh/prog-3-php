5d07a58eb2ca586e395b14bb4868e78453fa266b
GianfrancoCh <gianfrancochiarizia@gmail.com>
2024-05-19 21:10:19 -0300

    ejercicio venta

--- Clase04/Venta.php
+++ Clase04/Venta.php
@@ -1,5 +1,4 @@
 <?php
-
 class Venta{
     public $codigoBarra;
     public $nombre;
@@ -40,11 +39,59 @@ class Venta{
         return $id;
     }
 
-    public static function verificarProductoUsuario($codigoBarra,$idUsuario){
-        
+    public static function validarUsuario($idUsuario){
+
+        $filePath = 'usuarios.json';
+        $usuarios = [];
+        if (file_exists($filePath)) {
+            $usuarios = json_decode(file_get_contents($filePath), true);
+            foreach ($usuarios as $usuario) {
+                if ($usuario['id'] == $idUsuario){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
+    public static function validarProducto($codigoBarra){
+
+        $filePath = 'productos.json';
+        $productos = [];
+        if (file_exists($filePath)) {
+            $productos = json_decode(file_get_contents($filePath), true);
+            foreach ($productos as $producto) {
+                if ($producto['codigobarra'] == $codigoBarra){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static function validarStock($cantidad){
+
+        $filePath = 'productos.json';
+        $productos = [];
+        if (file_exists($filePath)) {
+            $productos = json_decode(file_get_contents($filePath), true);
+            foreach ($productos as $producto) {
+                if ($producto['stock'] >= $cantidad){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static function validarVenta($codigoBarra,$idUsuario,$cantidad){
+
+        if(self::validarUsuario($idUsuario) && self::validarProducto($codigoBarra) && self::validarStock($cantidad)){
+            return true;
 
 
+        }
+
+    }
+    
 
 }
--- Clase04/realizarVenta.php
+++ Clase04/realizarVenta.php
@@ -8,7 +8,7 @@ include("Venta.php");
 if(isset($_POST["codigo"]) && isset($_POST["id"]) && isset($_POST["cantidad"])){
 
 
-    if(Venta::verificarProductoUsuario($_POST["codigo"],$_POST["id"])){
+    if(Venta::validarVenta($_POST["codigo"],$_POST["id"],$_POST["cantidad"])){
 
         echo "Venta Realizada";
 
